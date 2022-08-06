dd243d86fedee863e6d363204b50e534f1ecfd29
alikenova312 <alikenova312@icloud.com>
2022-08-06 19:52:40 +0600

    lesson5

---
+++ main.js
@@ -0,0 +1,70 @@
+// let i = 1;
+// while (i < 10){
+//     console.log(i)
+//     i++
+// }
+
+
+// let i = 1;
+// do {
+//     console.log(i++)
+// }while (i <= 10)
+
+// let world = 'abraCadabra'
+// for (let i = 0; i < Math.floor(world.length / 2); i++){
+// }
+
+// let world = 'abraCadabra'
+// let worldRevers = ''
+// for (let i = 0; i < world.length; i++){
+//     worldRevers = world[i] + worldRevers
+// }
+// console.log(worldRevers)
+
+// let world = 'abraCadabra'
+// for (let i = 0; i < world.length; i++){
+//     if (i % 2){
+//         console.log(world[i])
+//     }
+// }
+
+// let world = ['a', 'b', 'c', 'd', 'e', 'f', 'j']
+// for (let i = 0; i < world.length; i++){
+//     if (i % 2){
+//         world[i] = world[i].toUpperCase()
+//     }else {
+//         world[i] = world[i].toLowerCase()
+//     }
+// }
+//
+// console.log(world)
+
+// let car = ['bmw', 'lexus', 'mers', 'honda','subaru']
+// for (let i = 0; i < car.length; i++){
+//     if (car[i].length >= 4){
+//         console.log(car[i])
+//     }
+// }
+
+
+// let arr = [[1,2], [3,4], [5,6]]
+//
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i][0] + arr[i][0])
+// }
+
+
+// let words = ['abra', 'max', 'mers', 'apple']
+//
+// for (let i = 0; i < words.length; i++){
+// }
+
+// let numbers = [1, 2, 3, 4, 7, 9, 20, 56, 40]
+// let result = 0;
+//
+// for (let i = 0; i < numbers.length; i ++){
+//
+// }
+
+
+
